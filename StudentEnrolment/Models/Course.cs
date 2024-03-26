0d2e07993f2be7b6261ab6b26afd42e3ba3a5ded
AbebeGetnet <aabizy@gmail.com>
2024-03-26 08:31:55 -0700

    Update course to department relationship

--- StudentEnrolment/Models/Course.cs
+++ StudentEnrolment/Models/Course.cs
@@ -1,10 +1,17 @@
-﻿namespace StudentEnrolment.Models
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentEnrolment.Models
 {
     public class Course
     {
         public int Id { get; set; }
         public string CourseTitle { get; set; }
         public int CreditHour { get; set; }
+        public int DepartmentId { get; set; }
+        [ForeignKey(nameof(DepartmentId))]
+        [ValidateNever]
+        public virtual Department Department { get; set; }
         public virtual ICollection<Student> Students { get; set; }
         public virtual ICollection<Enrolment> Enrolments { get; set; }
     }
