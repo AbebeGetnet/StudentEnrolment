65752209ce07315d59912edcb64fc296f884fdd7
AbebeGetnet <aabizy@gmail.com>
2024-04-14 08:25:28 -0700

    Add Campus CRUD

---
+++ StudentEnrolment/Controllers/CampusController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using StudentEnrolment.Data;
+using StudentEnrolment.Models;
+
+namespace StudentEnrolment.Controllers
+{
+    public class CampusController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CampusController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Campus
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Campuses.Include(c => c.College);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Campus/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Campuses == null)
+            {
+                return NotFound();
+            }
+
+            var campus = await _context.Campuses
+                .Include(c => c.College)
+                .FirstOrDefaultAsync(m => m.CampusId == id);
+            if (campus == null)
+            {
+                return NotFound();
+            }
+
+            return View(campus);
+        }
+
+        // GET: Campus/Create
+        public IActionResult Create()
+        {
+            ViewData["CollegeId"] = new SelectList(_context.Colleges, "CollegeId", "CollegeId");
+            return View();
+        }
+
+        // POST: Campus/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CampusId,Name,CollegeId")] Campus campus)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(campus);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CollegeId"] = new SelectList(_context.Colleges, "CollegeId", "CollegeId", campus.CollegeId);
+            return View(campus);
+        }
+
+        // GET: Campus/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Campuses == null)
+            {
+                return NotFound();
+            }
+
+            var campus = await _context.Campuses.FindAsync(id);
+            if (campus == null)
+            {
+                return NotFound();
+            }
+            ViewData["CollegeId"] = new SelectList(_context.Colleges, "CollegeId", "CollegeId", campus.CollegeId);
+            return View(campus);
+        }
+
+        // POST: Campus/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CampusId,Name,CollegeId")] Campus campus)
+        {
+            if (id != campus.CampusId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(campus);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CampusExists(campus.CampusId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CollegeId"] = new SelectList(_context.Colleges, "CollegeId", "CollegeId", campus.CollegeId);
+            return View(campus);
+        }
+
+        // GET: Campus/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Campuses == null)
+            {
+                return NotFound();
+            }
+
+            var campus = await _context.Campuses
+                .Include(c => c.College)
+                .FirstOrDefaultAsync(m => m.CampusId == id);
+            if (campus == null)
+            {
+                return NotFound();
+            }
+
+            return View(campus);
+        }
+
+        // POST: Campus/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Campuses == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Campuses'  is null.");
+            }
+            var campus = await _context.Campuses.FindAsync(id);
+            if (campus != null)
+            {
+                _context.Campuses.Remove(campus);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CampusExists(int id)
+        {
+          return (_context.Campuses?.Any(e => e.CampusId == id)).GetValueOrDefault();
+        }
+    }
+}
