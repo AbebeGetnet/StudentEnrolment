d014bfab6cd80c669c5f9a9c6e97bf17cc7e9f83
AbebeGetnet <aabizy@gmail.com>
2024-04-14 08:30:43 -0700

    Add CampusViewModel

---
+++ StudentEnrolment/Models/ViewModels/CampusViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace StudentEnrolment.Models.ViewModels
+{
+    public class CampusViewModel
+    {
+        public int CampusId { get; set; }
+        public int CollegeId { get; set; }
+        public virtual College? College { get; set; }
+        public string CollegeName { get; set; }
+    }
+}
